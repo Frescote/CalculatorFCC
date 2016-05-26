58266c4c15a1a8078d2c8c7463635a0032de052f
Frescote <totalvarez@gmail.com>
2016-05-26 20:19:44 +0100

    Ans

--- js/app.js
+++ js/app.js
@@ -2,80 +2,81 @@ $( document ).ready(function() {
     
     
     var displayStr = "";
+    var ans = "";
     
     function evalutateExpression(expression) {
         return eval(displayStr);
     }
     
     function updateDisplay (command) {
-        if(command === "Equals") {
-            displayStr = evalutateExpression(displayStr);
-            $('#screen').val(displayStr);
-        }else{
-            switch (command) {
-                case "0":
-                    displayStr += "0";
-                    break;
-                case "1":
-                    displayStr += "1";
-                    break;
-                case "2":
-                    displayStr += "2";
-                    break;
-                case "3":
-                    displayStr += "3";
-                    break;
-                case "4":
-                    displayStr += "4";
-                    break;
-                case "5":
-                    displayStr += "5";
-                    break;
-                case "6":
-                    displayStr += "6";
-                    break;
-                case "7":
-                    displayStr += "7";
-                    break;
-                case "8":
-                    displayStr += "8";
-                    break;
-                case "9":
-                    displayStr += "9";
-                    break;
-                case "Plus":
-                    displayStr += "+";
-                    break;
-                case "Minus":
-                    displayStr += "-";
-                    break;
-                case "Divide":
-                    displayStr += "/";
-                    break;
-                case "Multiply":
-                    displayStr += "*";
-                    break;
-                case "CE":
-                    displayStr = displayStr.slice(0,-1);
-                    break;
-                case "AC":
-                    displayStr = "";
-                    break;
-                case "IntDivision":
-                    displayStr += "%";
-                    break;
-                case "Decimal":
-                    displayStr += ".";
-                    break;
-                case "Ans":
-                    break;
-                default:
-                    displayStr = "ERROR";
-                    break;
-                    
-            }
-            $('#screen').val(displayStr);
+        switch (command) {
+            case "0":
+                displayStr += "0";
+                break;
+            case "1":
+                displayStr += "1";
+                break;
+            case "2":
+                displayStr += "2";
+                break;
+            case "3":
+                displayStr += "3";
+                break;
+            case "4":
+                displayStr += "4";
+                break;
+            case "5":
+                displayStr += "5";
+                break;
+            case "6":
+                displayStr += "6";
+                break;
+            case "7":
+                displayStr += "7";
+                break;
+            case "8":
+                displayStr += "8";
+                break;
+            case "9":
+                displayStr += "9";
+                break;
+            case "Plus":
+                displayStr += "+";
+                break;
+            case "Minus":
+                displayStr += "-";
+                break;
+            case "Divide":
+                displayStr += "/";
+                break;
+            case "Multiply":
+                displayStr += "*";
+                break;
+            case "CE":
+                displayStr = displayStr.slice(0,-1);
+                break;
+            case "AC":
+                displayStr = "";
+                break;
+            case "IntDivision":
+                displayStr += "%";
+                break;
+            case "Decimal":
+                displayStr += ".";
+                break;
+            case "Equals":
+                displayStr = evalutateExpression(displayStr);
+                ans = displayStr;
+                break;
+            case "Ans":
+                displayStr = ans;
+                break;
+            default:
+                displayStr = "ERROR";
+                break;
+                
         }
+        $('#screen').val(displayStr);
     }
     
     $('#screen').val(displayStr);
